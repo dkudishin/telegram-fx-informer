9833489ddff5fbbedbc26ca1f7648ec6feae1b88
dkudishin <dkudishin@gmail.com>
2023-08-16 22:03:29 +0200

    change CI profile

--- src/main/java/dk/kudishin/telegramfxinformer/services/BotBootstrapService.java
+++ src/main/java/dk/kudishin/telegramfxinformer/services/BotBootstrapService.java
@@ -3,12 +3,15 @@ package dk.kudishin.telegramfxinformer.services;
 
 import dk.kudishin.telegramfxinformer.FxInformerBot;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Service
+//@ConditionalOnExpression("#{!environment.getProperty('spring.profiles.active').contains('ci')}")
+@Profile("!ci")
 public class BotBootstrapService implements CommandLineRunner {
 
 
